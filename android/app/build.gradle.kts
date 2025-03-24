fb4117ac5d3027d17731daadb7d6dd0fd2029404
mohamedelkheety <mohamedelkheaty97@gmail.com>
2025-03-24 03:15:23 +0200

    change app version release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 21
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
     signingConfigs {
         create("release") {
